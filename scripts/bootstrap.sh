925ecabd7e3e94fcd1db01507b74530a5dfda479
nitrologic <nitrologic@gmail.com>
2018-01-03 01:13:18 +0000

    bump linux bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,8 @@
 mkdir ../bin
-
+cp ../env/*.txt ../bin
 cp ~/monkey2/bin/mx2cc_linux ../bin
 
 mx2cc=../bin/mx2cc_linux
 
-$mx2cc makemods -clean -config=release monkey libc std
-
-# $mx2cc makeapp -clean -apptype=console -config=release -product=bin/mx2cc.exe ../mx2cc/mx2cc.monkey2
+#$mx2cc makemods -clean -config=release monkey libc std
+$mx2cc makeapp -clean -apptype=console -config=release -product=bin/mx2cc ../mx2cc/mx2cc.monkey2
